27ff0ade09e2bc7ad1876eb6686141018052e3c7
jfarmer91 <farmerjong@gmail.com>
2015-12-24 14:50:49 -0500

    Overlay-Layout.html Centered

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-function fadeOut(x) {
-  
-}
+
+// $(document).ready(function(){
+//   $("#edupont-overlay").ready(function(){
+//     document.getElementById("#initial-overlay").style.width = screen.width;
+//     document.getElementById("#initial-overlay").style.height = screen.height;
+//   });
+// });
